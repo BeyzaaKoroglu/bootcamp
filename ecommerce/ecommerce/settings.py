f16b72d70eebca4c6d9e7e51fe3a0345477dc96c
Beyza Koroglu <beyza.koroglu.97@gmail.com>
2021-12-04 02:12:42 +0300

    lesson-two

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -42,7 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "django_extensions"
+    "django_extensions",
+    "core",
+    "customers",
+    "products"
 ]
 
 MIDDLEWARE = [
@@ -134,3 +137,4 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = "customers.Customer"
